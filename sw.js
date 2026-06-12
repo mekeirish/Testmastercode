ab665121d5c3e89ca2c20b8b269887556fa77791
mekeirish <mmm1747lb@gmail.com>
2026-06-12 19:41:17 +0200

    Hubcode: fast UP update via clipboard (sw.js)

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
-const cacheName='Mastercode-v1'; self.addEventListener('install',e=>{e.waitUntil(caches.open(cacheName).then(c=>c.addAll(['./index.html','./manifest.json','./icon.png'])));}); self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const cacheName = 'mastercode-pwa-v1';
+
+// Installation : Mise en cache locale obligatoire
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll([
+        'index.html',
+        'manifest.json',
+        'icon.png'
+      ]);
+    })
+  );
+});
+
+// Interception des requêtes : Stratégie Cache-First
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
